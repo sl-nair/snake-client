49c9c90c5d220d865a1fbfce80cbd492f8cc0c29
Sethu Nair <sethunair2102@gmail.com>
2023-04-21 17:59:58 -0600

    added setupInput function

--- play.js
+++ play.js
@@ -3,3 +3,22 @@ const { connect } = require('./client')
 // establishes a connection with the game server
 console.log("Connecting ...");
 connect();
+
+ const handleUserInput = function(key) {
+    process.stdin.write(key);
+    if (key === '\u0003') {
+    process.exit();
+  }}
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+ 
+
+  return stdin;
+};
+
+setupInput()
