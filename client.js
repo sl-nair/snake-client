8e3e010bbf9875b1d4a79516f676dd5959cd2e05
Sethu Nair <sethunair2102@gmail.com>
2023-04-20 19:11:08 -0600

    moved connect function from play.js to client.js

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Connected");
+  });
+  
+  conn.on('data', (message) => {
+    console.log(message)
+  })
+
+  return conn;
+};
+
+
+module.exports = { connect };
