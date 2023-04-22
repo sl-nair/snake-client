9ea6b232e84aea7de1535f49fa0264a3e9651983
Sethu Nair <sethunair2102@gmail.com>
2023-04-21 19:24:18 -0600

    moved constants to contants.js

---
+++ constants.js
@@ -0,0 +1,14 @@
+const IP = 'localhost';
+const PORT = 50541;
+
+const messages = {
+  '1': "Say: You can run but you cant hide",
+  '2': "Say: I'm gonna get u!!",
+  '3': "Say: Why are u running",
+};
+
+module.exports = {
+  IP,
+  PORT,
+  messages
+};
--- input.js
+++ input.js
@@ -1,12 +1,6 @@
-const { connect } = require("http2");
+const { messages } = require('./constants')
 
 let connection;
-
-const messages = {
-  '1': "Say: You can run but you cant hide",
-  '2': "Say: I'm gonna get u!!",
-  '3': "Say: Why are u running",
-}
 
 const handleUserInput = function (key) {
   if (key === '\u0003') {
@@ -24,7 +18,6 @@ const handleUserInput = function (key) {
     connection.write(messages[key])
   }
 };
-
 
 const setupInput = function (conn) {
   connection = conn;
